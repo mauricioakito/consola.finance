f18ee62618ca0ff8d97320e14011dcd194b7e8d7
Mauricio Akito <mauricio94jap@hotmail.com>
2022-11-18 10:19:31 -0300

    refact(Tables): Removed unused style call

--- src/components/Tables/TableAssets.ts
+++ src/components/Tables/TableAssets.ts
@@ -3,6 +3,5 @@ import "./styles/bootstrap.min.css";
 import "./styles/font-awesome.min.css";
 import "./styles/animate.css";
 import "./styles/select2.min.css";
-// import "./styles/perfect-scrollbar.css";
 import "./styles/util.css";
 import "./styles/main.css";
