7e2dc0273e9fa359bffa6d3a24669407a7f0838e
Mauricio Kitazawa <61957945+mauricioakito@users.noreply.github.com>
2022-11-21 10:12:35 -0300

    feat(Filter): Added filter (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,55 @@
 import { useEffect, useState } from "react";
-import { GetData } from "./utils/GetData";
+import { getData } from "./utils/getData";
 import { Table } from "./components/Tables/Table";
 import classNames from "classnames";
 import { RequestTypes } from "./Types/Request";
 import { ConfirmedSignatureInfo } from "@solana/web3.js";
 
 function App() {
-  const [data, setData] = useState<RequestTypes[] | ConfirmedSignatureInfo[]>();
-  const [isLoading, setIsLoading] = useState<Boolean>(false);
+    const [data, setData] = useState<RequestTypes[] | ConfirmedSignatureInfo[]>();
+    const [isLoading, setIsLoading] = useState<Boolean>(false);
 
-  const handleFetch = async () => {
-    setIsLoading(true);
-     const oldData = data && data;
-    const newFetchedData = await GetData();
-    if (newFetchedData && oldData) {
-      const newData: any = [...oldData, ...newFetchedData];
-      setData(newData);
-      setIsLoading(false);
-    }
-  };
+    const handleFetch = async () => {
+        setIsLoading(true);
+        const oldData = data && data;
+        const newFetchedData = await getData();
 
-  useEffect(() => {
-    GetData()
-      .then((value: ConfirmedSignatureInfo[]) => {
-        setData(value);
-      })
-      .catch((error) => console.error(error));
-  }, []);
+        const unique: RequestTypes[] = [];
 
-  if (!data) return null;
+        if (newFetchedData && oldData) {
+            const newData: any = [...oldData.reverse(), ...newFetchedData.reverse()];
 
-  return (
-    <div className="App">
-      <Table data={data} />
-      <button
-        onClick={handleFetch}
-        className={classNames("fetchMore", {
-          ["isLoading"]: isLoading,
-        })}
-      >
-        {isLoading ? "Loading..." : "Fetch more transactions"}
-      </button>
-    </div>
-  );
+            newData.forEach((item: RequestTypes) => 
+                unique.filter(uniqueItem => uniqueItem.slot === item.slot).length > 0 ? null : unique.push(item));
+        
+        setData(unique);
+        setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getData()
+        .then((value: ConfirmedSignatureInfo[]) => {
+            setData(value);
+        })
+        .catch((error) => console.error(error));
+    }, []);
+
+    if (!data) return null;
+
+    return (
+        <div className="App">
+        <Table data={data} />
+        <button
+            onClick={handleFetch}
+            className={classNames("fetchMore", {
+            ["isLoading"]: isLoading,
+            })}
+        >
+            {isLoading ? "Loading..." : "Fetch more transactions"}
+        </button>
+        </div>
+    );
 }
 
 export default App;
